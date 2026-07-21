743bea700af772e204bb5a61bb8a07b580c4fdbb
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-07-21 16:44:29 -0400

    fix build

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,9 +1,10 @@
 import { createWorkerFetch } from 'cloudflare'
 import { LinearTokenStore } from 'cloudflare/linear-token-store'
+import { GithubTokenStore } from 'cloudflare/github-token-store'
 import { type BotEnv } from './env'
 import { linearOAuthHandler } from './linear-oauth'
 
-export { LinearTokenStore }
+export { LinearTokenStore, GithubTokenStore }
 
 const fetchHandler = createWorkerFetch<BotEnv>({
   ...linearOAuthHandler,
